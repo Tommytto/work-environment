be67c73439f1e7540e271249310623f3103cc22b
tommytto <t.togyzbaev@gmail.com>
2019-06-16 23:42:09 +0700

    small fixes and tests

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const files = require('./lib/files');
-const yargs = require('yargs');
 const {askAction, ACTION} = require('./lib/inquirer');
 const {setupLinter} = require('./src/linting/linting');
 
+clear();
 // Start
-// console.log(chalk.blue(figlet.textSync('workio', {horizontalLayout: 'full'})));
+console.log(chalk.blue(figlet.textSync('WorkEnv', {horizontalLayout: 'full'})));
 const actionConfig = {
     [ACTION.LINTER]: setupLinter,
 };
