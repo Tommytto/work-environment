624ba1e7b54a7b6e642c448baeb5c450918c86c1
tommytto <t.togyzbaev@gmail.com>
2019-08-03 19:20:36 +0700

    deps fix

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const clear = require('clear');
 const figlet = require('figlet');
 const {askAction, ACTION} = require('./lib/inquirer');
 const {setupLinter} = require('./src/linting/linting');
+const {setupGitRepository} = require('./src/git/git');
 
 clear();
 // Start
 console.log(chalk.blue(figlet.textSync('WorkEnv', {horizontalLayout: 'full'})));
 const actionConfig = {
     [ACTION.LINTER]: setupLinter,
+    [ACTION.INIT_GIT]: setupGitRepository,
 };
 askAction().then(async ({settingsList}) => {
     for (let i = 0; i < settingsList.length; i++) {
---
+++ src/git/git.js
@@ -0,0 +1,7 @@
+function setupGitRepository() {
+
+}
+
+module.exports = {
+    setupGitRepository
+}
